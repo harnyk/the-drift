d7aa798f38fb44fc4be000f37f859700ce50c0c1
Mark <markharnyk@gmail.com>
2025-06-15 18:57:10 +0200

    fix: update homepage in package.json and improve UiOverlay component

--- src/UiOverlay.tsx
+++ src/UiOverlay.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import pkg from '../package.json';
 
 /**
  * Transparent full-screen overlay used for React UI elements.
  * Currently only displays a small debug label.
  */
 const UiOverlay: React.FC = () => (
-  <div className="w-full h-full pointer-events-none">
-    <div className="absolute bottom-0 right-0 p-1 text-xs text-gray-500">
-      created by TheDrift team
+    <div className="w-full h-full">
+        <div className="absolute bottom-0 right-0 p-1 text-xs text-gray-500">
+            <a target="_blank" href={pkg.homepage}>
+                TheDrift v{pkg.version}
+            </a>
+        </div>
     </div>
-  </div>
 );
 
 export default UiOverlay;
